00625074ddc9c001620ceddd74f17f572e58a902
Vinicius Quaresma da Luz <28585648+vinql@users.noreply.github.com>
2020-03-22 10:10:40 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/daluz/com/doomsday_register/MainActivity.kt
@@ -0,0 +1,12 @@
+package daluz.com.doomsday_register
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
